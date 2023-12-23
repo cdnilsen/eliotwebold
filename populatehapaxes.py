f7ef199b07f2f49815f697d8093946e5035eb58a
unknown <campbell.nilsen@gmail.com>
2023-12-23 13:10:32 -0500

    1 John proofread in full

--- populatehapaxes.py
+++ populatehapaxes.py
@@ -1,3 +1,19 @@
 from wordfrequencymodule import populateHapaxes
 
 populateHapaxes()
+
+allHapaxes = open("hapaxLegomenaStrict.txt", "r", encoding="utf-8").readlines()
+
+allChars = []
+allCharCountDict = {}
+for line in allHapaxes:
+    for char in line.strip():
+        if char not in allChars:
+            allChars.append(char)
+            allCharCountDict[char] = 1
+        else:
+            allCharCountDict[char] += 1
+
+allChars.sort(key=lambda x: allCharCountDict[x], reverse=True)
+for char in allChars:
+    print(char + ": " + str(allCharCountDict[char]))
