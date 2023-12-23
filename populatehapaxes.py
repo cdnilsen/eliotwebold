45f1f550e19ede1a0376aa0ee32583695d0be979
unknown <campbell.nilsen@gmail.com>
2023-12-23 15:17:56 -0500

    various fixes

--- populatehapaxes.py
+++ populatehapaxes.py
@@ -1,19 +1,26 @@
 from wordfrequencymodule import populateHapaxes
 
+showChars = input("Show all characters in hapaxes? (y/n) ")
+
 populateHapaxes()
 
-allHapaxes = open("hapaxLegomenaStrict.txt", "r", encoding="utf-8").readlines()
+if showChars == "y":
 
-allChars = []
-allCharCountDict = {}
-for line in allHapaxes:
-    for char in line.strip():
-        if char not in allChars:
-            allChars.append(char)
-            allCharCountDict[char] = 1
-        else:
-            allCharCountDict[char] += 1
+    allHapaxes = open("hapaxLegomenaStrict.txt", "r", encoding="utf-8").readlines()
 
-allChars.sort(key=lambda x: allCharCountDict[x], reverse=True)
-for char in allChars:
-    print(char + ": " + str(allCharCountDict[char]))
+    allChars = []
+    allCharCountDict = {}
+    for line in allHapaxes:
+        for char in line.strip():
+            if char not in allChars:
+                allChars.append(char)
+                allCharCountDict[char] = 1
+            else:
+                allCharCountDict[char] += 1
+
+    allChars.sort(key=lambda x: allCharCountDict[x], reverse=True)
+    for char in allChars:
+        print(char + ": " + str(allCharCountDict[char]))
+
+else:
+    quit()
